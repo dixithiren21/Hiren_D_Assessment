df376ff97323d95af3bb522252204ddd254a1ca2
Hiren <hiren.d@sgit.in>
2019-10-29 15:44:11 +0530

    Init upload

---
+++ app/src/main/java/com/info/hirend_assessment/MainActivity.java
@@ -0,0 +1,21 @@
+package com.info.hirend_assessment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.info.hirend_assessment.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/info/hirend_assessment/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.info.hirend_assessment.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
